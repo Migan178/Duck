cbad4b29c701bbeb8c70e24ee23748a0f1edd9ca
siwoo131 <tigersiwoo131@gmail.com>
2021-07-17 21:49:14 +0900

    two commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Client } from 'discord.js';
 import Dokdo from "dokdo";
-const { TOKEN } = require('../config.json');
 
+const { TOKEN } = require('../config.json');
 const client = new Client();
 const prefix = "!";
 const DokdoHandler = new Dokdo(client, { aliases: ['dokdo', 'dok', "독도", "debug", "debugging"], prefix: '!', noPerm: (message) => message.reply("어라? 당신은 개발자가 아닌데요?") });
@@ -14,6 +14,6 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.author.bot) return;
-    DokdoHandler.run(msg);
+    DokdoHandler.run(msg).then();
     if (msg.content === `${prefix}ping`) return msg.reply("pong");
 });

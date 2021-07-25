44c5f3048b9cc15e9cc55e407ab942e3c3df3c01
siwoo131 <tigersiwoo131@gmail.com>
2021-07-25 18:42:34 +0900

    무지성 commandHandler

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
-import { Client } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import { Client, Collection } from 'discord.js';
 import Dokdo from "dokdo";
+import { PathLike, readdirSync } from 'fs';
 
-const { TOKEN } = require('../config.json');
 const client = new Client();
-const prefix = "!";
+const prefix: string = "!";
 const DokdoHandler = new Dokdo(client, { aliases: ['dokdo', 'dok', "독도", "debug", "debugging"], prefix: '!', noPerm: (message) => message.reply("어라? 당신은 개발자가 아닌데요?") });
 
-client.login(TOKEN).then();
+let client_commands: any = new Collection();
+
+client.login(process.env.TOKEN).then();
 
 client.on('ready', () => {
     console.log("봇");
 });
 
+let client_commands_load = (dir: PathLike) => {
+    for (const file of readdirSync(dir)) {
+        const cmd = require(`./commands/${file}`);
+        client_commands.set(cmd.name, cmd);
+    }
+    console.log(client_commands.map((c: { name: any; }) => c.name).join(', ') + ' Load Success');
+}
+
+client_commands_load(__dirname + "/commands");
+
+
 client.on('message', msg => {
     if (msg.author.bot) return;
     DokdoHandler.run(msg).then();
-    if (msg.content === `${prefix}ping`) return msg.reply("pong");
+    if (!msg.content.startsWith(prefix)) return;
+    if (msg.content.slice(0, prefix.length) !== prefix) return;
+
+
+    const args: string[] = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const shift: any = args.shift();
+    const command = shift.toLowerCase();
+
+    let cmd: any = client_commands.get(command);
+
+    if (cmd) cmd.run(client, msg, args);
 });

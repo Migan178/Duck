09517bb599dca8bc1f2097fd8373e47888499f7d
siwoo131 <tigersiwoo131@gmail.com>
2021-07-25 20:31:46 +0900

    코드 개편

---
+++ src/Bot.ts
@@ -0,0 +1,78 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { Client, Collection } from 'discord.js';
+import Dokdo from 'dokdo';
+import { readdirSync } from 'fs';
+
+const prefix: string = "!"; // 여기 있는 prefix 변경
+const client = new Client();
+const DokdoNoPerm = (msg: any) => {
+    msg.reply("당신은 개발자가 아니라 해당 명령어를 수행할수 없습니다.");
+};
+const DokdoHandler = new Dokdo(client,
+    {
+        aliases: ['dokdo', 'dok'],
+        prefix: `${prefix}`,
+        noPerm: DokdoNoPerm
+    }
+);
+
+function botSetActivity() {
+    client.user!.setActivity('개발중', { type: "PLAYING" });
+}
+
+async function ownerDmSend() {
+    const ownersId: string = "415135882006495242"; // 여기에 있는 id 는 수정해 주셔야 합니다.
+    const owners = await client.users.fetch(ownersId);
+    await owners.send('Bot ready');
+};
+
+function botLogin() {
+    client.login(process.env.TOKEN).then();
+}
+
+function botReady() {
+    client.on('ready', () => {
+        console.log(`Login ${client.user!.username}`);
+        botSetActivity();
+        ownerDmSend();
+    });
+};
+
+let client_commands: any = new Collection();
+
+let client_commands_load = (dir: any) => {
+    for (const file of readdirSync(dir)) {
+        const cmd = require(`./commands/${file}`);
+        client_commands.set(cmd.name, cmd);
+    }
+    console.log(client_commands.map((c: { name: any; }) => c.name).join(', ') + ' Load Success');
+}
+
+client_commands_load(__dirname + "/commands");
+
+
+async function botCommands() {
+    client.on('message', msg => {
+        if (msg.author.bot) return;
+        DokdoHandler.run(msg);
+        if (!msg.content.startsWith(prefix)) return;
+        if (msg.content.slice(0, prefix.length) !== prefix) return;
+    
+    
+        const args: string[] = msg.content.slice(prefix.length).trim().split(/ +/g);
+        const shift: any = args.shift();
+        const command = shift.toLowerCase();
+    
+        let cmd: any = client_commands.get(command);
+    
+        if (cmd) cmd.run(client, msg, args);
+    });
+
+}
+
+export {
+    botReady,
+    botLogin,
+    botCommands
+};
--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,6 +1,14 @@
+import { MessageEmbed } from 'discord.js';
+
 module.exports = {
-    name: "ping",
-    run(client: any, message: any, args: any) {
-        message.reply("pong");
+    name: "핑",
+    run: async (client: any, message: any, args: any) => {
+        const Embed = new MessageEmbed()
+            .setTitle(":ping_pong:퐁!")
+            .setDescription(`웹소켓 핑: \`${client.ws.ping}\`ms\n메세지 핑: \`${Date.now() - message.createdTimestamp}\`ms`)
+            .setColor(0x000000)
+            .setTimestamp(Date.now())
+            .setFooter(message.author.tag, message.author.displayAvatarURL());
+        message.channel.send(Embed);
     }
 }

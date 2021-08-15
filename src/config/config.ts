b87000ccdd569effaa1a4311695b4721609db1d8
siwoo131 <tigersiwoo131@gmail.com>
2021-08-15 15:16:38 +0900

    .

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,8 @@
 const prefix: string = "!";
 const owners: string = "415135882006495242"; // 여기에 있는 id 는 수정해 주셔야 합니다.
-const config = { prefix, owners };
+const config: { prefix: string; owners: string | string[] } = {
+  prefix,
+  owners,
+};
 
 export default config;
